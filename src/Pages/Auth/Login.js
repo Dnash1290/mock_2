2e9a026a5cf1fbd4742432682248eebef43d8a32
Dnash1290 <nashestev@gmail.com>
2025-01-16 12:06:30 +0000

    backend front end login

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,10 +1,33 @@
 import "./Auth.css"
 import authImg from "../../Components/Images/flower_auth.jpg"
 import { useState } from "react"
+import { useContext } from "react"
+import { Context } from "../../UseContextData/Data"
+import axios from "axios"
 
 export default function Login(){
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState ("")
+
+    const {login} = useContext(Context)
+
+    function fetch_login(event){
+        event.preventDefault()
+        const ENDPOINT = "http://localhost:8001/auth/login"
+        const data = {
+            "username":username,
+            "password":password
+        }
+        axios.post(ENDPOINT,data)
+        .then(response => {
+            login(response.data)
+            alert("you have log in")
+        })
+        .catch(response =>{
+            
+            alert(response.Error);
+        })
+    } 
 
     return(
         <div className="background">
@@ -12,12 +35,12 @@ export default function Login(){
                 <img src={authImg}/>
                 <div id="auth">
                     <div className="auth-head-text">Log in</div>
-                    <form>
+                    <form onSubmit={fetch_login}>
                         <div className="auth-input-text">Username
                             <br/>
                         <input
                             className="input-class"
-                            type="email"
+                            type="text"
                             placeholder="Username/ Email"
                             value={username}
                             onChange={(e)=> setUsername(e.target.value)}
@@ -33,7 +56,7 @@ export default function Login(){
                             onChange={(e)=> setPassword(e.target.value)}
                         />
                         </div>
-                        <button className="auth-input-button" onClick={() =>alert([username, password])}>Login</button>
+                        <button className="auth-input-button" type="Submit">Login</button>
                     </form>
                     <div id="auth-nav-links">
                         <a href="/" className="auth-nav">Forgot Password?</a>
